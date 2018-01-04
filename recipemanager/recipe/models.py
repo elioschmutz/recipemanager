5a3032cde6b88560e12aeb699ce37d1aab084674
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-01-04 22:46:12 +0100

    Add recipe app

---
+++ recipemanager/recipe/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Recipe(models.Model):
+    """
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='recipe_images')
+    www = models.URLField()
+    duration = models.IntegerField()
+    portions = models.IntegerField()
+    preparation = models.TextField()
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.title
